5255a00a81e0af0548a8438f3ced4816459b4931
saheeb_tareque <saheebtareque97@gmail.com>
2023-08-14 23:53:17 +0600

    Finished implenting the code for the program to work

--- index.py
+++ index.py
@@ -1,3 +1,28 @@
-playerchoice = input("Enter your favourite number between 1-3:\n")
-print(type(playerchoice))
+import sys
+import random
+from enum import Enum
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSOR = 3
+
+print("")
+playerchoice = input("Enter..\n 1.For Rock \n 2.For Paper \n 3.For Scissor \n")
 player = int(playerchoice)
+if player < 1 or player > 3:
+    sys.exit("Please You must enter between 1-3:")
+print("Player choice is: " + str(RPS(player)).replace("RPS.","") + ".")
+computerchoice = random.choice("123")
+computer = int(computerchoice)
+print("Computer choice is:", str(RPS(computer)).replace("RPS.","")+ ".")
+
+if player == 1 and computer == 3:
+    print("You win")
+elif player == 2 and computer == 1:
+    print("You win")
+elif player == 3 and computer == 2:
+    print("You win")
+elif player == computer:
+    print("It is a draw game")
+else:
+    print("You lost the game")
